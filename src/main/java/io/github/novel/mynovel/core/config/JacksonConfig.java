f0dee4fbcc819a9fcf6d7ed83f6012f779420c63
Zixiao Zhou <efreet364@gmail.com>
2026-06-24 10:11:13 +0800

    fix: 新增jackson配置文件修复前端js对Long类型数字精度丢失的问题

---
+++ src/main/java/io/github/novel/mynovel/core/config/JacksonConfig.java
@@ -0,0 +1,21 @@
+package io.github.novel.mynovel.core.config;
+
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    /*
+    * Long类型数字都转化为字符串，避免前端js精度丢失
+    * */
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jackson2ObjectMapperBuilderCustomizer() {
+        return builder -> {
+            builder.serializerByType(Long.class, ToStringSerializer.instance);
+            builder.serializerByType(Long.TYPE, ToStringSerializer.instance);
+        };
+    }
+}
