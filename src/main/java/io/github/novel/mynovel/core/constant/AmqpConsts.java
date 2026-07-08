e8c8e750bbd28c9a0c9f7021422e279e258abf4b
Zixiao Zhou <efreet364@gmail.com>
2026-07-08 15:26:04 +0800

    feat: 新增rabbitmq实时更新小说es索引信息功能

---
+++ src/main/java/io/github/novel/mynovel/core/constant/AmqpConsts.java
@@ -0,0 +1,22 @@
+package io.github.novel.mynovel.core.constant;
+
+public class AmqpConsts {
+
+    /**
+     * 小说信息改变 MQ
+     */
+    public static class BookChangeMq {
+
+        /**
+         * 小说信息改变交换机
+         */
+        public static final String EXCHANGE_NAME = "EXCHANGE-BOOK-CHANGE";
+
+        /**
+         * Elasticsearch book 索引更新的队列
+         */
+        public static final String QUEUE_ES_UPDATE = "QUEUE-ES-BOOK-UPDATE";
+
+
+    }
+}
---
+++ src/test/java/io/github/novel/mynovel/MQTest.java
@@ -0,0 +1,40 @@
+package io.github.novel.mynovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MQTest {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testSimpleQueue() {
+        // 队列名称
+        String queueName = "simple.queue";
+        // 消息
+        String message = "hello, spring amqp!";
+        // 发送消息
+        rabbitTemplate.convertAndSend(queueName, message);
+    }
+
+    /**
+     * workQueue
+     * 向队列中不停发送消息，模拟消息堆积。
+     */
+    @Test
+    public void testWorkQueue() throws InterruptedException {
+        // 队列名称
+        String queueName = "work.queue";
+        // 消息
+        String message = "hello, message_";
+        for (int i = 0; i < 50; i++) {
+            // 发送消息，每20毫秒发送一次，相当于每秒发送50条消息
+            rabbitTemplate.convertAndSend(queueName, message + i);
+            Thread.sleep(20);
+        }
+    }
+}
